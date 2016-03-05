dfd6ae521f717f3d46d7cba6e066dbfbca628bec
k-jin <kjin@u.northwestern.edu>
2016-03-04 22:48:39 -0600

    starting table class

--- table.cc
+++ table.cc
@@ -14,6 +14,77 @@ ostream & Table::Print(ostream &os) const
 #endif
 
 #if defined(DISTANCEVECTOR)
-	std::vector<vector<double>> neighbors;
+	//TODO: Initiliazed all rows in a table to be empty vector<double>
+	Table::Table(unsigned pN, unsigned nN, std::vector<std::vector<double>> rT) : 
+		parentNode(pN), numNodes(nN), routingTable(rT) {}
+		
+	Table::Table(unsigned pN, unsigned nN) : 
+		parentNode(pN), numNodes(nN) {}
+	
+	Table::Table()
+	{}
+	
+	Table::Table(const Table &rhs) :
+		parentNode(rhs.parentNode), numNodes(rhs.numNodes), routingTable(rhs.routingTable) {}
+		
+	Table & Table::operator=(const Table &rhs)
+	{
+		return *(new (this) Table(rhs));
+	}
+	
+	Table::~Table()
+	{}
+	
+	
+	virtual bool RowMatches(const std::vector<double> compare, const unsigned nodeNum) const
+	{
+		if (compare.size() != numNodes) {
+			return false;
+		}
+		std::vector<double> tableRow = routingTable[nodeNum];
+		
+		for (int i = 0; i < numNodes; i++) {
+			if (compare[i] != tableRow[i]) { return false; }
+		}
+		return true;
+	}
+	
+	ostream & Print(ostream &os) const
+	{
+		os << "Routing Table for node " << parentNode << endl;
+		unsigned rtSize = routingTable.size();
+		
+		for(int i = 0; i < rtSize; i++) {
+			std::vector<double> currRow = routingTable[i];
+			unsigned rowSize = currRow.size();
+			if (rowSize > 0) {
+				os << "Neighbor node " << i << "\t";
+				for(int j = 0; j < rowSize; j++){
+					os << "Destination node: " << j << "/Distance: " << currRow[j] << "\t"; 
+				}
+				os << endl;
+			}
+		}
+		return os;
+		
+	}
+	
+	virtual void SetParentNode(unsigned nodeNum) { parentNode = nodeNum; }
+	virtual unsigned GetParentNode() const {return parentNode;}
+	virtual void SetNumNodes(unsigned number) { numNodes = number; }
+	virtual unsigned GetNumNodes() const { return numNodes; }
+	virtual void SetRow(unsigned neighborNum, std::vector<double> row) 
+	{
+		routingTable[neighborNum] = row;
+	}
+	virtual std::vector<double> GetRow(unsigned neighborNum) const { return routingTable[neighborNum]; }
+	virtual void SetEntry(unsigned neighborNum, unsigned destNum, double distance)
+	{
+		routingTable[neighborNum][destNum] = distance;
+	}
+	virtual double GetEntry(unsigned neighborNum, unsigned destNum) const
+	{
+		return routingTable[neighborNum][destNum];
+	}
 		
 #endif
--- table.h
+++ table.h
@@ -30,16 +30,36 @@ class Table {
 #include <vector>
 
 class Table {
+	// parent node of routing table
+	unsigned parentNode;
+	// total number of nodes in the system
+	// also length of each internal vector<double>
+	unsigned numNodes;
 	// index of outer vector would be indexed by neighbors and self
 	// index of inner vectors would be indexed by every node in the system
-	std::vector<vector<double>> routingTable;
+	// initialize the table such that every vector<double> is length 0
+	// initialize rows such that every distance is -1 to represent infinity
+	std::vector<std::vector<double>> routingTable;
+	
  public:
+	Table(unsigned pN, unsigned nN, std::vector<std::vector<double>> rT);
+	Table(unsigned pN, unsigned nN);
+	Table();
+	Table(const Table &rhs);
+	Table & operator=(const Table &rhs);
+	virtual ~Table();
+	
+	virtual bool RowMatches(const std::vector<double> compare, const unsigned nodeNum) const;
+	
 	ostream & Print(ostream &os) const;
-	virtual void initTable(int size);
-	virtual void SetRow(unsigned neighborNum, vector<double> row);
-	virtual vector<double> GetRow(unsigned neighborNum) const;
+	virtual void SetParentNode(unsigned nodeNum);
+	virtual unsigned GetParentNode() const;
+	virtual void SetNumNodes(unsigned number);
+	virtual unsigned GetNumNodes() const;
+	virtual void SetRow(unsigned neighborNum, std::vector<double> row);
+	virtual std::vector<double> GetRow(unsigned neighborNum) const;
 	virtual void SetEntry(unsigned neighborNum, unsigned destNum, double distance);
-	virtual double GetEntry(unsigned neighborNum, unsigned destNum);
+	virtual double GetEntry(unsigned neighborNum, unsigned destNum) const;
 };
 #endif
 
