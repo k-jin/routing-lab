6779ef8114a16ec0528c42ac5201c0ced803d638
k-jin <kjin@u.northwestern.edu>
2016-03-08 04:43:59 -0600

    fixed pointer issues in dv LinkHasBeenUpdated

--- node.cc
+++ node.cc
@@ -154,10 +154,10 @@ bool Node::Matches(const Node &rhs) const
     // update our table
     // send out routing mesages
     cerr << *this<<": Link Update: "<<*l<<endl;
-    Table *table = GetRoutingTable();
-    map<unsigned, map<unsigned, double> > forwardingTable = *table.GetForwardingTable();
+    Table* table = GetRoutingTable();
+    map<unsigned, map<unsigned, double> > forwardingTable = table->GetForwardingTable();
     map<unsigned, map<unsigned, double> >::iterator tableIt = forwardingTable.begin();
-    Link newLink = *link;
+    Link newLink = *l;
     unsigned destId = newLink.GetDest();
     double newLat = newLink.GetLatency();
 
